f0e62c2ccc1ca2c80f0d651f25211ccb1c0d597c
ElshadowRAT <adbielcenmail.com>
2024-09-21 15:30:24 -0600

    starting login and regiser

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -127,3 +127,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'ecommerce/webapp/static')]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'index.html'
--- ecommerce/webapp/admin.py
+++ ecommerce/webapp/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Usuario, Producto, Pedido, ArticuloPedido, Pago, Reseña
+from .models import Producto, Pedido, ArticuloPedido, Pago, Reseña
 # Register your models here.
 
-admin.site.register(Usuario)
 admin.site.register(Producto)
 admin.site.register(Pedido)
 admin.site.register(ArticuloPedido)
