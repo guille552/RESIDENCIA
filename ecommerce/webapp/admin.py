182b0da77fba9926fd0a1af7a9c7777fbcf8320a
Denni <146395573+dennidc@users.noreply.github.com>
2024-09-26 08:53:58 -0600

    actualizacion

--- ecommerce/webapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Producto, Pedido, ArticuloPedido, Pago, Reseña
-# Register your models here.
-
-admin.site.register(Producto)
-admin.site.register(Pedido)
-admin.site.register(ArticuloPedido)
-admin.site.register(Pago)
-admin.site.register(Reseña)
